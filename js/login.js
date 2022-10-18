3d4662e09f7fcbeb552a32cfeb35ee7c2074e33e
Diego Daniel Jofré Winterstetter <diegojofre7489@gmail.com>
2022-10-17 22:33:16 -0300

    Ultimas modificaciones

--- js/login.js
+++ js/login.js
@@ -1,32 +1,71 @@
-let usuarios = []
-function login(event) {
-    
-    event.preventDefault();
-    console.log(event);
-    
-    let formLogin = document.querySelector("#formulario")
-    console.dir(formLogin)
-    
-    let pine = formLogin[0].value
-    let password = formLogin[1].value
-    let pinDB = " "
-    let passwordDB = " "
-    let k = 0
-    
-    if (usuarios.indexOf(pine) && usuarios.indexOf(password)) {
-        pinDB = pine    
-        passwordDB = password
-    }
-    
-    if (usuarios.includes(pinDB) || usuarios.includes(passwordDB)) {
-        if (pinDB.trim() && passwordDB.trim()) Swal.fire('Ha ingresado con éxito!', '', 'success')
-        else Swal.fire('Credenciales incorrectas', '', 'info')
-        
-    }else Swal.fire('Credenciales incorrectas', '', 'info')
-    
-    
-    
-}
+// class Usuario {
+//     constructor(nombre, apellido, correo, contrasena) {
+//         this.nombre = nombre
+//         this.apellido = apellido
+//         this.correo = correo
+//         this.contrasena = contrasena
 
+//     }
+// }
+
+// let usuarios = []
+// usuarios.push(7489, "ahm")
+// // usuarios.push(1234,"die")
+// // usuarios.push(74123, "1234")
+let vista = document.querySelectorAll('#ver')
 let submitLogin = document.querySelector("#enviar")
 submitLogin.addEventListener("click", login)
+
+function login(event) {
+
+    event.preventDefault();
+    console.log(event);
+
+    let formLogin = document.querySelector("#formulario")
+    
+    let traer_ls = JSON.parse(localStorage.getItem("usuario"))
+        traer_ls.forEach(e => {
+                if (formLogin[0].value == e.correo && formLogin[1].value == e.contrasena) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: '¡Has ingresado con éxito! Volverás al inicio.   ',
+                        showConfirmButton: false,
+                        timer: 5000
+                    })
+                    setTimeout(function () { window.location.href = "../index.html"; }, 3000);
+                    vista.forEach(e =>{
+                    e.id = 'nover'
+                    })
+                    
+                }else{  
+                    Swal.fire({
+                        icon: 'warning',
+                        title: '¡Las credenciales no coinciden! ',
+                        showConfirmButton: true,
+                    })
+                }
+            })
+        
+
+
+    
+
+
+
+
+
+
+    // if (usuarios.indexOf(correo) && usuarios.indexOf(password)) {
+    //     pinDB = pine
+    //     passwordDB = password
+    // }
+
+    // if (usuarios.includes(pinDB) || usuarios.includes(passwordDB)) {
+    //     if (pinDB.trim() && passwordDB.trim()) Swal.fire('Ha ingresado con éxito!', '', 'success')
+    //     else Swal.fire('Credenciales incorrectas', '', 'info')
+
+    // } else Swal.fire('Credenciales incorrectas', '', 'info')
+
+
+
+}
--- js/register.js
+++ js/register.js
@@ -1,83 +1,95 @@
+id = 1
 class Usuario {
     constructor(nombre, apellido, correo, contrasena) {
         this.nombre = nombre
         this.apellido = apellido
         this.correo = correo
         this.contrasena = contrasena
-
+        this.id = id++
     }
 }
+  
 
 let usuarios = []
-// usuarios.push(7489, "ahm")
-// usuarios.push(1234,"die")
-// usuarios.push(74123, "1234")
 
 let submitRegister = document.querySelector("#submitRegister")
 submitRegister.addEventListener("click", registrarUsuario)
 
 function registrarUsuario(event) {
-    
-    sessionStorage.removeItem(1)
-    
+
     event.preventDefault();
-    console.log(event);
-    
+
     let formRegister = document.querySelector("#formulario")
-    console.dir(formRegister)
-    
+
     let nombre = formRegister[0].value
     let apellido = formRegister[1].value
     let correo = formRegister[2].value
     let contraseñax = formRegister[3].value
     let contraseñas = formRegister[4].value
     let b = false
-    
+
+    if (formRegister[0].value == null || formRegister[0].value.length == 0) {
+        alert('¡El nombre no puede estar vacío!')
+    }
+    if (formRegister[1].value == null || formRegister[1].value.length == 0) {
+        alert('¡El apellido no puede estar vacío!')
+    }
+    if (formRegister[2].value == null|| formRegister[2].value.length == 0) {
+        alert('¡El correo no puede estar vacío!')
+    }
+    if (formRegister[3].value == null || formRegister[3].value.length == 0) {
+        
+        alert('¡La contraseña debe contener al menos 8 caracteres y no puede estar vacía!')
+    }
+    if (formRegister[4].value == null || formRegister[4].value.length == 0) {
+       alert('¡Repita su contraseña!')
+    }
+
     do {
-        
-        (contraseñax == contraseñas) ? b = true : alert("Los valores no coinciden. Intente nuevamente2"), b = true
-        
+
+        if (contraseñax == contraseñas) {
+
+            b = true
+
+        } else {
+
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Las Contraseñas no coinciden! ',
+                showConfirmButton: true,
+            })
+            b = true
+
+        }
+
     } while (b != true);
 
-    console.log(contraseñas)
-    console.log(contraseñax)
-    
-    
-    if ( contraseñax == contraseñas) {
-        
-        usuarios.push(new Usuario(nombre, apellido, correo, contraseñas))
-        
-    }
 
-    const token = generarToken(nombre, contraseñax)
-    sessionStorage.setItem("token", token)
 
-    const autenticado = existToken()
-    if (autenticado && contraseñax == contraseñas) {
+    if (formRegister[3].value.length >=8) {
+        if (contraseñax == contraseñas || !formRegister[3].value == null || !formRegister[4].value == null) {
+
+            usuarios.push(new Usuario(nombre, apellido, correo, contraseñas))
+            localStorage.setItem("usuario", JSON.stringify(usuarios));
+            Swal.fire({
+                icon: 'success',
+                title: '¡Te has registrado con éxito! Volverás al inicio.   ',
+                showConfirmButton: false,
+                timer: 5000
+            })
+            setTimeout(function () { window.location.href = "../index.html"; }, 3000);
+    
+        }
+        
+    }else{
         Swal.fire({
-            icon: 'success',
-            title: '¡Te has registrado con éxito! Volverás al inicio.   ',
-            showConfirmButton: false,
-            timer: 5000
-          })
-          setTimeout( function() { window.location.href = "../index.html"; }, 3000 );
-        
+            icon: 'warning',
+            title: '¡La contraseña debe tener al menos 8 caracteres!',
+            showConfirmButton: true,
+        })
     }
-
-    console.log(usuarios)
-
-}
+    
 
 
-function generarToken(nombre, contrasena) {
-    return nombre + contrasena
-}
-
-function existToken() {
-    if (sessionStorage.getItem("token") !== null) {
-        return true
-    } else {
-        return false
-    }
 }
 
